1c5e33825ac116881c0dbdd9e4cbccb8f1f5cc7a
Michael Ekstrand <ekstrand@txstate.edu>
2014-10-24 17:11:37 -0500

    update analyze.R

--- analyze.R
+++ analyze.R
@@ -3,5 +3,5 @@ library(ggplot2)
 eval.results = read.csv('eval-results.csv')
 
 png('results.png')
-qplot(Algorithm, RMSE.ByRating, data=eval.results, geom='boxplot')
+qplot(Algorithm, RMSE.ByUser, data=eval.results, geom='boxplot')
 dev.off()
